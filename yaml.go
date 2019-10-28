5476556989605916178a2b64a7effefdbffe870c
Kee2013 <chinboy2012@gmail.com>
2019-10-28 23:17:45 +0800

    修正错误

--- yaml.go
+++ yaml.go
@@ -1,9 +1,3 @@
-/*************************************************************************
-   > File Name: yaml.go
-   > Author: Kee
-   > Mail: chinboy2012@gmail.com
-   > Created Time: 2019.10.25
-************************************************************************/
 package kiris
 
 import (
@@ -36,25 +30,25 @@ func NewYamlLoad(filename string) *Yaml {
 		log.Fatal("Unmarshal Yaml: ", e)
 	}
 
-	cnf = ExpandValueEnvForMap(cnf)
+	cnf = FormatValueMaps(cnf)
 	return &Yaml{data: cnf, filename: filename}
 }
 
-func ExpandValueEnvForMap(m map[string]interface{}) map[string]interface{} {
+func FormatValueMaps(m map[string]interface{}) map[string]interface{} {
 	// 获取keys
 	for k, v := range m {
 		switch value := v.(type) {
 		case string:
 			m[k] = ExpandValueEnv(value)
 		case map[string]interface{}:
-			m[k] = ExpandValueEnvForMap(value)
+			m[k] = FormatValueMaps(value)
 		case map[interface{}]interface{}:
 			_value := make(map[string]interface{})
 			for _k, _v := range m[k].(map[interface{}]interface{}) {
 				_value[_k.(string)] = _v
 			}
 			m[k] = _value
-			m[k] = ExpandValueEnvForMap(_value)
+			m[k] = FormatValueMaps(_value)
 		case map[string]string:
 			for k2, v2 := range value {
 				value[k2] = ExpandValueEnv(v2)
@@ -67,36 +61,37 @@ func ExpandValueEnvForMap(m map[string]interface{}) map[string]interface{} {
 
 // Convert `$(ENV)` || `$(ENV||defaultValue)` || `$(ENV||)`
 // Return the env value || if env is nil return defaultValue || env is nil return ""
-func ExpandValueEnv(value string) (realValue string) {
-	realValue = value
+func ExpandValueEnv(value string) string {
+	rVal := value
 
-	defaultV := ""
+	dVal := ""
 	regx := regexp.MustCompile(`(?U)\$\{.+\}`)
-	if x := regx.FindAllString(realValue, -1); len(x) > 0 {
+
+	if x := regx.FindAllString(rVal, -1); len(x) > 0 {
 		for _, v := range x {
-			vLen := len(v)
-			if vLen < 3 {
+			vL := len(v)
+			if vL < 3 {
 				continue
 			}
-			if key := v[2 : vLen-1]; len(key) > 0 {
-				defVal := ""
-				defValIndex := strings.Index(v, "||")
-				if defValIndex > 0 {
-					key = v[2:defValIndex]
-					defVal = v[defValIndex+2 : vLen-1]
+			if key := v[2 : vL-1]; len(key) > 0 {
+				dValal := ""
+				dValalIndex := strings.Index(v, "||")
+				if dValalIndex > 0 {
+					key = v[2:dValalIndex]
+					dValal = v[dValalIndex+2 : vL-1]
 				}
 
-				eVal := GetEnv(key, defVal).(string)
-				realValue = strings.Replace(realValue, v, eVal, -1)
+				eVal := GetEnv(key, dValal).(string)
+				rVal = strings.Replace(rVal, v, eVal, -1)
 			}
 		}
 	}
 
-	if realValue == "" {
-		realValue = defaultV
+	if rVal == "" {
+		rVal = dVal
 	}
 
-	return
+	return Ternary(rVal == "", dVal, rVal).(string)
 }
 
 func (this *Yaml) Get(key string, _def ...interface{}) interface{} {
