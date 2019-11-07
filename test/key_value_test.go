5fb4c97b7050d0fe388ce97ac6dd6cbb276e1c7a
Kee2013 <chinboy2012@gmail.com>
2019-11-07 08:09:21 +0800

    添加key-value

---
+++ test/key_value_test.go
@@ -0,0 +1,44 @@
+package kiris
+
+import (
+	"fmt"
+	"testing"
+)
+
+var KvMaps = NewK_VMaps()
+var saveFile = "./data/dump.kvmap"
+
+func TestSet(t *testing.T) {
+	name := KvMaps.Set("Name", "Kee")
+	sex := KvMaps.Set("Sex", 1)
+	age := KvMaps.Set("Age", 28)
+	addr := KvMaps.Set("Addr", "China, Fujian, Xiamen")
+	name.SetAttr("next", sex)
+	sex.SetAttr("next", age)
+	age.SetAttr("next", addr)
+	addr.SetAttr("next", nil)
+
+	fmt.Println(name, "\n", sex, "\n", age, "\n", addr, "\n", sex.GetAttr("next"))
+	fmt.Println(StrPad("", "->", 100, KIRIS_STR_PAD_RIGHT))
+}
+
+func TestPrint(t *testing.T) {
+	KvMaps.Del("Age")
+	KvMaps.Set("Name", "Marco")
+
+	KvMaps.Print()
+
+	fmt.Printf("Print Age Value (Defalut 22): %d \n", KvMaps.GetValue("Age", 22))
+	KvMaps.Set("Age", 26)
+	fmt.Printf("Print Age Value (= 26): %d \n", KvMaps.GetValue("Age"))
+	e := KvMaps.Save(saveFile)
+	fmt.Println("Key-Value Save: ", e)
+}
+
+func TestLoad(t *testing.T) {
+	fmt.Println(StrPad("", "=", 100, KIRIS_STR_PAD_RIGHT))
+	fmt.Printf("+%50s%-48s+\n", "TEST LOAD DB FILE", "")
+	kMap := NewK_VMaps()
+	kMap.Load(saveFile)
+	kMap.Print()
+}
--- test/utils_test.go
+++ test/utils_test.go
@@ -28,3 +28,12 @@ func TestTernary(t *testing.T) {
 	valC = true
 	fmt.Println(Ternary(valC, valA, Ternary(valA != valB, "Hello world", "Hi Kee")))
 }
+
+func TestStrPad(t *testing.T) {
+	s1 := StrPad("Hello World (Left)", "你好世界", 100, KIRIS_STR_PAD_LEFT)
+	s2 := StrPad("你好世界 (Right)", "=", 100, KIRIS_STR_PAD_RIGHT)
+	s3 := StrPad(" Hello World ", "Both", 100, KIRIS_STR_PAD_BOTH)
+	fmt.Printf("strPad > %s, LEN: %d \n", s1, StrCount(s1))
+	fmt.Printf("strPad > %s, LEN: %d \n", s2, StrCount(s2))
+	fmt.Printf("strPad > %s, LEN: %d \n", s3, StrCount(s3))
+}
